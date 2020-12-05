329134d52dc6909a1fc394ff1ee6013e447be071
Mateusz Teteruk <mateusz.teteruk@gmail.com>
2020-12-05 21:51:26 +0100

    Add query params to GetRecentTracks.

--- app/src/main/java/pl/mateuszteteruk/lastfmlibrary/recenttracks/domain/GetRecentTracks.kt
+++ app/src/main/java/pl/mateuszteteruk/lastfmlibrary/recenttracks/domain/GetRecentTracks.kt
@@ -1,11 +1,16 @@
 package pl.mateuszteteruk.lastfmlibrary.recenttracks.domain
 
-import pl.mateuszteteruk.lastfmlibrary.core.dataaccess.dto.RecentTrackDto
+import pl.mateuszteteruk.lastfmlibrary.recenttracks.dataaccess.dto.RecentTrackDto
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface GetRecentTracks {
 
-    @GET("")
-    suspend fun getRecentTracks(): RecentTrackDto
+    @GET("?method=user.getRecentTracks&format=json")
+    suspend fun getRecentTracks(
+        @Query("user") user: String,
+        @Query("api_key") apiKey: String,
+        @Query("limit") limit: Int = 15
+    ): RecentTrackDto
 
 }
