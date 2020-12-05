dd8a96c0a3124fd347688484ea36593421c3087e
Mateusz Teteruk <mateusz.teteruk@gmail.com>
2020-12-05 21:30:04 +0100

    Add Hilt. Prepare NetworkModule with OkHttp and Retrofit providing.

---
+++ app/src/main/java/pl/mateuszteteruk/lastfmlibrary/di/module/NetworkModule.kt
@@ -0,0 +1,33 @@
+package pl.mateuszteteruk.lastfmlibrary.di.module
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .build()
+
+    @Singleton
+    @Provides
+    fun provideRetrofitClient(okHttpClient: OkHttpClient): Retrofit =
+        Retrofit.Builder()
+            .client(okHttpClient)
+            .baseUrl("https://ws.audioscrobbler.com/2.0/")
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+
+}
