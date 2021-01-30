19610cebcd7d30b44c88d7abff862defd9f7a220
Mateusz Teteruk <mateusz.teteruk@gmail.com>
2021-01-30 20:38:27 +0100

    Add FragmentScope, annotate with it RecentTracksComponent

---
+++ core/src/main/kotlin/pl/mateuszteteruk/core/di/scope/FragmentScope.kt
@@ -0,0 +1,8 @@
+package pl.mateuszteteruk.core.di.scope
+
+import javax.inject.Scope
+import kotlin.annotation.AnnotationRetention.RUNTIME
+
+@Scope
+@Retention(RUNTIME)
+annotation class FragmentScope
--- recenttracks/src/main/java/pl/mateuszteteruk/recenttracks/di/component/RecentTracksComponent.kt
+++ recenttracks/src/main/java/pl/mateuszteteruk/recenttracks/di/component/RecentTracksComponent.kt
@@ -1,9 +1,11 @@
 package pl.mateuszteteruk.recenttracks.di.component
 
 import dagger.Subcomponent
+import pl.mateuszteteruk.core.di.scope.FragmentScope
 import pl.mateuszteteruk.recenttracks.presentation.SecondFragment
 
 @Subcomponent
+@FragmentScope
 interface RecentTracksComponent {
 
     @Subcomponent.Factory
